6beabea32331ec14da42d28ae1ecda533c2d4d8d
Apoorv Bedmutha <i-apoorv@hushh.ai>
2024-07-07 12:51:46 +0530

    edited comments

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -7,11 +7,9 @@ exports.routes = (app) => {
   app.route("/").get((req, res) => {
     res.send("App is working");
   });
-
-  // for creation of a new account
+  
   app.route("/getLocations").post(async (req, res) => res.send(await services.getLocations(req, res)));
 
-  // for creation of a new account
   app.route("/foodSearch").post(async (req, res) => {
     let locationID=req.body["locationID"];
     res.send(await services.FoodItems(locationID))
